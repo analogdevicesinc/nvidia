1a75b76ca869d5f04f679ad847ad6f9ebd81f84a
Cosmin Tanislav <demonsingur@gmail.com>
2023-03-17 17:21:58 +0200

    vi: go through all subdevice pads

--- drivers/media/platform/tegra/camera/vi/channel.c
+++ drivers/media/platform/tegra/camera/vi/channel.c
@@ -1910,11 +1910,12 @@ int tegra_channel_init_subdevices(struct tegra_channel *chan)
 
 	sd->grp_id = grp_id;
 	chan->grp_id = grp_id;
-	index = pad->index - 1;
-	while (index >= 0) {
+	while (index < entity->num_pads) {
 		pad = &entity->pads[index];
-		if (!(pad->flags & MEDIA_PAD_FL_SINK))
-			break;
+		if (!(pad->flags & MEDIA_PAD_FL_SINK)) {
+			index++;
+			continue;
+		}
 
 		pad = media_entity_remote_pad(pad);
 		if (pad == NULL || !tegra_is_v4l2_subdev(pad->entity))
@@ -1933,8 +1934,6 @@ int tegra_channel_init_subdevices(struct tegra_channel *chan)
 			"vi-output", sd->name);
 		if (len < 0)
 			return -EINVAL;
-
-		index = pad->index - 1;
 	}
 	spec_bar(); /** for num_sd < MAX_SUBDEVICES */
 
