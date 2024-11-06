c5d34945d215241ef7b3f13a7c15547d0ef546ec
Ketan Patil <ketanp@nvidia.com>
2024-11-06 13:13:19 +0000

    video: tegra: nvmap: Remove use of iommu_present API
    Upstream has removed iommu_present function from k6.13, hence remove
    call to this function from nvmap, as IOMMU will be always present in
    any latest SOC. We will rely on dont-convert-iovmm-to-carveout property
    from DT to avoid converting the entire IOVMM to carveout.
    
    Bug 4923950
    
    Change-Id: Ia7e374e81ab8c78f3d17187aebd2fcf0170fabdc
    Signed-off-by: Ketan Patil <ketanp@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3243903
    (cherry picked from commit b2e4b5813ff0dddcb1f126157e54749a9420edcc)
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3524199
    Reviewed-by: Brad Griffis <bgriffis@nvidia.com>
    Tested-by: Jon Hunter <jonathanh@nvidia.com>
    Reviewed-by: Jon Hunter <jonathanh@nvidia.com>
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>

--- drivers/video/tegra/nvmap/nvmap_dev.c
+++ drivers/video/tegra/nvmap/nvmap_dev.c
@@ -1379,25 +1379,6 @@ static void nvmap_iovmm_debugfs_init(void)
 	}
 }
 
-static bool nvmap_is_iommu_present(void)
-{
-	struct device_node *np;
-	struct property *prop;
-
-	np = of_find_node_by_name(NULL, "iommu");
-	while (np) {
-		prop = of_find_property(np, "status", NULL);
-		if (prop && !strcmp(prop->value, "okay")) {
-			of_node_put(np);
-			return true;
-		}
-		of_node_put(np);
-		np = of_find_node_by_name(np, "iommu");
-	}
-
-	return false;
-}
-
 int __init nvmap_probe(struct platform_device *pdev)
 {
 	struct nvmap_platform_data *plat;
@@ -1504,11 +1485,8 @@ int __init nvmap_probe(struct platform_device *pdev)
 			generic_carveout_present = 1;
 
 	if (generic_carveout_present) {
-		if (!iommu_present(&platform_bus_type) &&
-			!nvmap_is_iommu_present())
-			nvmap_convert_iovmm_to_carveout = 1;
-		else if (!of_property_read_bool(pdev->dev.of_node,
-				"dont-convert-iovmm-to-carveout"))
+		if (!of_property_read_bool(pdev->dev.of_node,
+			"dont-convert-iovmm-to-carveout"))
 			nvmap_convert_iovmm_to_carveout = 1;
 	} else {
 		nvmap_convert_carveout_to_iovmm = 1;
