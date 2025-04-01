7bca2b2466d234900a09cfb511b6dba23aef4bf1
Jerry Chang <jerchang@nvidia.com>
2025-04-01 16:49:38 +0800

    tegra: rtcpu: revise copyright
    incorrect use of NVIDIA Properitary License in Linux driver code
    
    Bug 5197152
    
    Change-Id: Id23b8cbd4fd6315664c77d7f42014206d1ef8d5c
    Signed-off-by: Jerry Chang <jerchang@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3330623
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: Jon Hunter <jonathanh@nvidia.com>
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>

--- drivers/platform/tegra/rtcpu/capture-ivc-priv.h
+++ drivers/platform/tegra/rtcpu/capture-ivc-priv.h
@@ -1,18 +1,8 @@
-/* SPDX-License-Identifier: LicenseRef-NvidiaProprietary
- *
- * SPDX-FileCopyrightText: Copyright (c) 2017-2025 NVIDIA CORPORATION.
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2017-2025 NVIDIA CORPORATION & AFFILIATES.
  *                         All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
  */
-
 #ifndef __CAPTURE_IVC_PRIV_H__
 #define __CAPTURE_IVC_PRIV_H__
 
--- drivers/platform/tegra/rtcpu/capture-ivc.c
+++ drivers/platform/tegra/rtcpu/capture-ivc.c
@@ -1,19 +1,7 @@
-// SPDX-License-Identifier: LicenseRef-NvidiaProprietary
+// SPDX-License-Identifier: GPL-2.0-only
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2017-2025 NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2017-2025 NVIDIA CORPORATION & AFFILIATES.
  *                         All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <nvidia/conftest.h>
 
